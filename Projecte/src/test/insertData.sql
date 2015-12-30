2c91eead918c897d443858117f37c04d3b5e314a
Ester Lorente <ester.lorente@outlook.com>
2015-12-31 00:59:34 +0100

    SQL Inserts

--- Projecte/src/test/insertData.sql
+++ Projecte/src/test/insertData.sql
@@ -18,3 +18,16 @@ INSERT INTO Espectacle VALUES('Don Quijote', 10);
 INSERT INTO Espectacle VALUES('La Bella Durmiente', 10);
 INSERT INTO Espectacle VALUES('Sueño de una noche de verano', 10);
 INSERT INTO Espectacle VALUES('La Bailarina del Templo', 10);
+
+INSERT INTO Local VALUES('Gran Teatre del Liceu', 'Les Rambles, 51-59');
+INSERT INTO Local VALUES('Teatre Nacional de Catalunya', 'Plaça de les Arts, 1');
+INSERT INTO Local VALUES('Teatro Goya', 'C/ Joaquín Costa, 68');
+INSERT INTO Local VALUES('Teatre Lliure de Montjuïc', 'Plaça de Margarida Xirgu, 1');
+INSERT INTO Local VALUES('Teatre Victòria', 'Av. del Paral·lel, 67');
+INSERT INTO Local VALUES('Teatre Gaudí', 'C/ Sant Antoni Maria Claret, 120');
+INSERT INTO Local VALUES('Teatre Lliure de Gràcia', 'C/ del Monseny, 47');
+INSERT INTO Local VALUES('Almeria Teatre', 'C/ Sant Lluís, 64');
+INSERT INTO Local VALUES('Teatre Club Capitol', 'Les Rambles, 138');
+
+INSERT INTO Representacio VALUES('Gran Teatre del Liceu', 'nit', '26/01/2012', 100, 50, 'Cisne negro', 'Gran Teatre del Liceu', 'nit');
+INSERT INTO Representacio VALUES('Gran Teatre del Liceu', 'mati', '26/01/2012', 100, 50, 'Cenicienta', 'Gran Teatre del Liceu', 'nit');
