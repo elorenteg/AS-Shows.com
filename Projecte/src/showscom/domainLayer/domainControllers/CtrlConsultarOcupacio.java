9d5b52ec6e8d8310ddf877462a2a7b6e209b1475
paguto <pguizan@hotmail.com>
2015-12-30 20:40:37 +0100

    Update CtrlConsultarOcupacio.java

--- Projecte/src/showscom/domainLayer/domainControllers/CtrlConsultarOcupacio.java
+++ Projecte/src/showscom/domainLayer/domainControllers/CtrlConsultarOcupacio.java
@@ -1,5 +1,11 @@
 package showscom.domainLayer.domainControllers;
 
+import java.util.List;
+
 public class CtrlConsultarOcupacio {
 
+	public List<Object> consultaOcupacio(String nomL, String sessio, int nombreEspectadors) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
