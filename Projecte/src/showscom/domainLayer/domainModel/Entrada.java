87d9f8e65231a601190dc24a606b250cbd4f02be
dominguezlosa <dominguezlosa@users.noreply.github.com>
2016-01-03 11:33:24 +0100

    Anotacions hibernate a Entrada

--- Projecte/src/showscom/domainLayer/domainModel/Entrada.java
+++ Projecte/src/showscom/domainLayer/domainModel/Entrada.java
@@ -2,12 +2,26 @@ package showscom.domainLayer.domainModel;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Entrada")
 public class Entrada {
+	@Id
 	private String identificador;
+	@Column(name="dniClient")
 	private String dniClient;
+	@Column(name="nombreEspectadors")
 	private int nombreEspectadors;
+	@Column(name="data")
 	private Date data;
+	@Column(name = "preu")
 	private float preu;
+	// TODO indicar sólo guardamos sessio i nomLocal de representació
 	private Representacio representacio;
 	
 	public Entrada(String identificador, String dni, int nombreEspectadors, Date dAvui, float preuTotal, Representacio r, Object seients) {
@@ -22,5 +36,5 @@ public class Entrada {
 		// TODO seients type 
 	}
 	
-	
 }
+
