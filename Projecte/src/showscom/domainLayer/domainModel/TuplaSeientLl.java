627e952be46b3c81d3718faab7b31cb57b111469
paguto <pguizan@hotmail.com>
2016-01-03 12:23:05 +0100

    Create TuplaSeientLl.java

---
+++ Projecte/src/showscom/domainLayer/domainModel/TuplaSeientLl.java
@@ -0,0 +1,23 @@
+package showscom.domainLayer.domainModel;
+
+public class TuplaSeientLl {
+	private int fila;
+	private int columna;
+	
+	public int getFila(){
+		return fila;
+	}
+	
+	public void setFila(int fila){
+		this.fila = fila;
+	}
+	
+	public int getColumna(){
+		return columna;
+	}
+	
+	public void setColumna(int columna){
+		this.columna = columna;
+	}
+	
+}
