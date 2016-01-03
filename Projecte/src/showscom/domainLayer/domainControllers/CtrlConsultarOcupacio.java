9a6abbb578c1e02b3f3e86a82ba23813f16024a3
paguto <pguizan@hotmail.com>
2016-01-03 12:32:03 +0100

    Update CtrlConsultarOcupacio.java

--- Projecte/src/showscom/domainLayer/domainControllers/CtrlConsultarOcupacio.java
+++ Projecte/src/showscom/domainLayer/domainControllers/CtrlConsultarOcupacio.java
@@ -2,10 +2,18 @@ package showscom.domainLayer.domainControllers;
 
 import java.util.List;
 
+import showscom.domainLayer.dataInterface.ICtrlRepresentacio;
+import showscom.domainLayer.domainModel.Representacio;
+import showscom.domainLayer.domainModel.TuplaSeientLl;
+import showscom.domainLayer.factories.CtrlDataFactory;
+
 public class CtrlConsultarOcupacio {
 
-	public List<Object> consultaOcupacio(String nomL, String sessio, int nombreEspectadors) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<TuplaSeientLl> consultaOcupacio(String nomL, String sessio, int nombreEspectadors) {
+		CtrlDataFactory ctrlDataFact = CtrlDataFactory.getInstance();
+		ICtrlRepresentacio ctrlRepr = ctrlDataFact.getCtrlRepresentacio();
+		Representacio r = ctrlRepr.getRepresentacio(nomL, sessio);
+		List<TuplaSeientLl>oc = r.obteSeientsLliures(nombreEspectadors);
+		return oc;
 	}
 }
