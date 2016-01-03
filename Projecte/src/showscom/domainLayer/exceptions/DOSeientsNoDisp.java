560db203c626d9852166b97a5118cb1c59f2f313
paguto <pguizan@hotmail.com>
2016-01-03 13:02:29 +0100

    Create DOSeientsNoDisp.java

---
+++ Projecte/src/showscom/domainLayer/exceptions/DOSeientsNoDisp.java
@@ -0,0 +1,9 @@
+package showscom.domainLayer.exceptions;
+
+public class DOSeientsNoDisp extends Exception {
+	public static final long serialVersionUID = 1L;
+	
+	public DOSeientsNoDisp() {
+		super();
+	}
+}
