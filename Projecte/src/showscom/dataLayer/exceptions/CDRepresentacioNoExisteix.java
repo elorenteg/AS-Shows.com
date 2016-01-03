d3d3f6fb01d16009e94b3358725c0bff5d56627a
paguto <pguizan@hotmail.com>
2016-01-03 12:45:42 +0100

    Create CDRepresentacioNoExisteix.java

---
+++ Projecte/src/showscom/dataLayer/exceptions/CDRepresentacioNoExisteix.java
@@ -0,0 +1,10 @@
+package showscom.dataLayer.exceptions;
+
+public class CDRepresentacioNoExisteix extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public CDRepresentacioNoExisteix() {
+		super();
+	}
+}
