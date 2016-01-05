bc0b8b051e26c9e4940a9e0d32b63d6b2f06e02b
paguto <pguizan@hotmail.com>
2016-01-05 12:56:21 +0100

    Create ICtrlLocal.java

---
+++ Projecte/src/showscom/domainLayer/dataInterface/ICtrlLocal.java
@@ -0,0 +1,18 @@
+package showscom.domainLayer.dataInterface;
+
+import java.util.List;
+
+import showscom.dataLayer.exceptions.CDLocalNoExisteix;
+import showscom.domainLayer.domainModel.Local;
+
+
+
+public interface ICtrlLocal {
+	
+	public Local getLocal(String nomL) throws CDLocalNoExisteix;
+
+	public boolean existLocal(String nomL) throws CDLocalNoExisteix;
+
+	public List<Local> getAllLocal();
+	
+}
