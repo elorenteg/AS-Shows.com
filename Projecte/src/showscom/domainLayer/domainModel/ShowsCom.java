c0078e9e2d3c76f9e632197dba743a3f2190e08b
Ester Lorente <ester.lorente@outlook.com>
2016-01-10 19:47:21 +0100

    Hibernate ShowsCom (falta check en divisa i canvis)

--- Projecte/src/showscom/domainLayer/domainModel/ShowsCom.java
+++ Projecte/src/showscom/domainLayer/domainModel/ShowsCom.java
@@ -2,13 +2,46 @@ package showscom.domainLayer.domainModel;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.CollectionOfElements;
+
+@Entity
+@Table(name = "ShowsCom")
+@Check(constraints = "codiBanc > 0 AND comissio > 0")
 public class ShowsCom {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
 	private static ShowsCom instance = new ShowsCom();
+	@Column(name = "codiBanc")
 	private int codiBanc;
+	@Column(name = "nCompte")
 	private String numeroCompte;
+	@Column(name = "comissio")
 	private float comissio;
+	@Column(name = "divisa")
+	@Enumerated(EnumType.STRING)
 	private Moneda divisa;
+
+	@CollectionOfElements
+	@JoinTable(name="Canvis")
+	@Column(name = "canvi", nullable = false)
+	@Enumerated(EnumType.STRING)
 	private List<Moneda> canvis;
+
+	@Column(name = "idVenudes")
 	private int venudes;
 
 	/*
