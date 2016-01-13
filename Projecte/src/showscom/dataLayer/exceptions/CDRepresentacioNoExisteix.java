d779a8fd321c433415e95aa5b326a2bde62c7bbf
Ester Lorente <ester.lorente@outlook.com>
2016-01-13 01:03:53 +0100

    comentarios dataLayer.exceptions

--- Projecte/src/showscom/dataLayer/exceptions/CDRepresentacioNoExisteix.java
+++ Projecte/src/showscom/dataLayer/exceptions/CDRepresentacioNoExisteix.java
@@ -1,9 +1,16 @@
 package showscom.dataLayer.exceptions;
 
+/**
+ * Classe de gestió d'Excepcions. Gestiona l'excepció de no existència d'una
+ * Representació.
+ */
 public class CDRepresentacioNoExisteix extends Exception {
+	public static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Mètode creador de la classe que invoca a la constructora de la classe
+	 * Exception
+	 */
 	public CDRepresentacioNoExisteix() {
 		super();
 	}
