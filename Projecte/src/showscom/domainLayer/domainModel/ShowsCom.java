36528338857ed159d1299bef196b435e8aec2956
Ester Lorente <ester.lorente@outlook.com>
2016-01-13 18:15:34 +0100

    .

--- Projecte/src/showscom/domainLayer/domainModel/ShowsCom.java
+++ Projecte/src/showscom/domainLayer/domainModel/ShowsCom.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.SecondaryTables;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Check;
