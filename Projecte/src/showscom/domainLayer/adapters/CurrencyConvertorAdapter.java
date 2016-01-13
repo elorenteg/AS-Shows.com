c12d5ccab392d3b1e31b560a618e9c6113a43167
dani <daniel_dolo@hotmail.com>
2016-01-13 23:52:02 +0100

    Coment currencyConvertorAdapter

--- Projecte/src/showscom/domainLayer/adapters/CurrencyConvertorAdapter.java
+++ Projecte/src/showscom/domainLayer/adapters/CurrencyConvertorAdapter.java
@@ -4,8 +4,20 @@ import showscom.domainLayer.exceptions.DOServeiNoDisponible;
 import showscom.domainLayer.serviceLocator.ServiceLocator;
 import showscom.domainLayer.services.CurrencyConvertor;
 
+/**
+ * Adapter del servei de canvi de divisa per a realitzar el pagament. Permet
+ * desacoblar la implementació de les operacions d’un servei extern del nostre
+ * cas d’ús.
+ */
 public class CurrencyConvertorAdapter implements ICurrencyConvertorAdapter {
 
+	/**
+	 * Obté la conversió d'una divisa a una altra
+	 * @param from divisa que volem canviar
+	 * @param to divisa a la que volem canviar
+	 * @return conversió de les divises
+	 * @throws Exception si no s'ha trobat el servei o la resposta és errònia
+	 */
 	public float conversorRate(String divisa, String moneda) throws DOServeiNoDisponible {
 		ServiceLocator servLoc = ServiceLocator.getInstance();
 		CurrencyConvertor curConv = (CurrencyConvertor) servLoc.find("CurrencyConvertor");
--- Projecte/src/showscom/domainLayer/adapters/ICurrencyConvertorAdapter.java
+++ Projecte/src/showscom/domainLayer/adapters/ICurrencyConvertorAdapter.java
@@ -2,8 +2,19 @@ package showscom.domainLayer.adapters;
 
 import showscom.domainLayer.exceptions.DOServeiNoDisponible;
 
+/**
+ * Adapter del servei de canvi de divisa per a realitzar el pagament. Ens permet
+ * definir que metodes ha d'implementar qualsevol CurrencyConvertorAdapter
+ */
 public interface ICurrencyConvertorAdapter {
 
+	/**
+	 * Obté la conversió d'una divisa a una altra
+	 * @param from divisa que volem canviar
+	 * @param to divisa a la que volem canviar
+	 * @return conversió de les divises
+	 * @throws Exception si no s'ha trobat el servei o la resposta és errònia
+	 */
 	public float conversorRate(String divisa, String moneda) throws DOServeiNoDisponible;
 
 }
