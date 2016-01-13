bf7ffb6c22bed92794d26592627bc43baa81a893
dominguezlosa <dominguezlosa@users.noreply.github.com>
2016-01-13 17:50:36 +0100

    Update DOServeiNoDisponible.java

--- Projecte/src/showscom/domainLayer/exceptions/DOServeiNoDisponible.java
+++ Projecte/src/showscom/domainLayer/exceptions/DOServeiNoDisponible.java
@@ -1,8 +1,16 @@
 package showscom.domainLayer.exceptions;
 
+/**
+ * Classe de gestió d'Excepcions. Gestiona l'excepció de no disponibilitat d'un
+ * Servei.
+ */
 public class DOServeiNoDisponible extends Exception {
 	public static final long serialVersionUID = 1L;
 
+	/**
+	 * Mètode creador de la classe que invoca a la constructora de la classe
+	 * Exception
+	 */
 	public DOServeiNoDisponible() {
 		super();
 	}
