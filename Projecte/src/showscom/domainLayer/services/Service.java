5cb470213ada7aacc26c7fa1b2b57221c82bd78e
dani <daniel_dolo@hotmail.com>
2016-01-13 19:31:43 +0100

    interface serveis

--- Projecte/src/showscom/domainLayer/services/Service.java
+++ Projecte/src/showscom/domainLayer/services/Service.java
@@ -1,7 +1,14 @@
 package showscom.domainLayer.services;
 
+/**
+ * Interface dels serveis
+ */
 public interface Service {
 
+	/**
+	 * Consulta el nom del servei
+	 * @return nom del servei
+	 */
 	public String getName();
 
 }
