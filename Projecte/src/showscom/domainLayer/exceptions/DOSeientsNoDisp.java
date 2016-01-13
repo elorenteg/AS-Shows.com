9887c375f02037767589c75c132e6208c491c4dc
dominguezlosa <dominguezlosa@users.noreply.github.com>
2016-01-13 17:50:18 +0100

    Update DOSeientsNoDisp.java

--- Projecte/src/showscom/domainLayer/exceptions/DOSeientsNoDisp.java
+++ Projecte/src/showscom/domainLayer/exceptions/DOSeientsNoDisp.java
@@ -1,8 +1,16 @@
 package showscom.domainLayer.exceptions;
 
+/**
+ * Classe de gestió d'Excepcions. Gestiona l'excepció que el nombre
+ * d’espectadors és més gran que el nombre de seients lliures.
+ */
 public class DOSeientsNoDisp extends Exception {
 	public static final long serialVersionUID = 1L;
 
+	/**
+	 * Mètode creador de la classe que invoca a la constructora de la classe
+	 * Exception
+	 */
 	public DOSeientsNoDisp() {
 		super();
 	}
