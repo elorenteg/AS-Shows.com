5f7c8c8af4698e718f5a05450bce3f9259c1ec8a
dominguezlosa <dominguezlosa@users.noreply.github.com>
2016-01-13 17:50:02 +0100

    Update DOPagamentNoAutoritzat.java

--- Projecte/src/showscom/domainLayer/exceptions/DOPagamentNoAutoritzat.java
+++ Projecte/src/showscom/domainLayer/exceptions/DOPagamentNoAutoritzat.java
@@ -1,8 +1,15 @@
 package showscom.domainLayer.exceptions;
 
+/**
+ * Classe de gestió d'Excepcions. Gestiona l'excepció de pagament no autoritzat.
+ */
 public class DOPagamentNoAutoritzat extends Exception {
 	public static final long serialVersionUID = 1L;
 
+	/**
+	 * Mètode creador de la classe que invoca a la constructora de la classe
+	 * Exception
+	 */
 	public DOPagamentNoAutoritzat() {
 		super();
 	}
