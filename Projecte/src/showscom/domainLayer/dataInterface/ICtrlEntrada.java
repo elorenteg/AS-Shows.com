5c6dc67ce1f4027225f86d47eba07be6ff1e5565
dominguezlosa <dominguezlosa@users.noreply.github.com>
2016-01-13 20:02:49 +0100

    Update ICtrlEntrada.java

--- Projecte/src/showscom/domainLayer/dataInterface/ICtrlEntrada.java
+++ Projecte/src/showscom/domainLayer/dataInterface/ICtrlEntrada.java
@@ -3,9 +3,25 @@ package showscom.domainLayer.dataInterface;
 import showscom.dataLayer.exceptions.CDEntradaNoExisteix;
 import showscom.domainLayer.domainModel.Entrada;
 
+/**
+ * Interface del controlador de la classe Entrada. Ens permet definir que
+ * metodes ha d'implementar qualsevol CtrlEntrada
+ *
+ */
 public interface ICtrlEntrada {
 
+	/**
+	 * Selecciona una Entrada identificada pel seu identificador guardada a la
+	 * BD
+	 * @param id identificador de l'Entrada
+	 * @return Entrada identificada per id
+	 * @throws CDEntradaNoExisteix si no existeix l'Entrada identificada per id
+	 */
 	public Entrada getEntrada(String id) throws CDEntradaNoExisteix;
 
+	/**
+	 * Inserta una Entrada a la BD
+	 * @param entrada Entrada a insertar
+	 */
 	public void insertEntrada(Entrada entrada);
 }
