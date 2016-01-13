9ea583361e33d1f783a93e359a7850ec09e2008b
dani <daniel_dolo@hotmail.com>
2016-01-13 20:21:23 +0100

    Comentari BankService

--- Projecte/src/showscom/domainLayer/services/BankService.java
+++ Projecte/src/showscom/domainLayer/services/BankService.java
@@ -3,12 +3,38 @@ package showscom.domainLayer.services;
 import java.util.Date;
 
 public class BankService implements Service {
+	// nom del servei
 	private static final String name = "BankService";
 
+	/**
+	 * Consulta el nom del servei
+	 * 
+	 * @return nom del servei
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Realitza la transferència de la quantitat monetaria establerta del compte
+	 * bancari del comprador al de l'agrupació
+	 * 
+	 * @param dni
+	 *            dni del comprador de l'entrada
+	 * @param codiB
+	 *            codi del banc del comprador
+	 * @param numCompte
+	 *            número de compte bancari del comprador
+	 * @param preu
+	 *            quantitat monetària a transferir
+	 * @param codiBShows
+	 *            codi del banc de l'agrupació
+	 * @param numCompteShows
+	 *            número de compte bancari de l'agrupació
+	 * @param dAvui
+	 *            data de realització de la transferencia
+	 * @return true si s'ha autoritzat la transferència, false altrament
+	 */
 	public boolean autoritza(String dni, int codiB, String numCompte, float preu, int codiBShows, String numCompteShows,
 			Date dAvui) {
 		return true;
