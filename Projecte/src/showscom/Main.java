3dc397276c8f010b6a096b058eff023f3166c418
Ester Lorente <ester.lorente@outlook.com>
2016-01-13 18:49:22 +0100

    comentaris Main

--- Projecte/src/showscom/Main.java
+++ Projecte/src/showscom/Main.java
@@ -2,8 +2,15 @@ package showscom;
 
 import showscom.presentationLayer.CtrlPresComprarEntrada;
 
+/**
+ * Programa principal
+ */
 public class Main {
 
+	/**
+	 * Funció principal que crida al Ctrl de Presentació per executar el Cas d'Ús de Compra d'Entrades
+	 * @param args Command-Line Arguments
+	 */
 	public static void main(String args[]) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
