d2c136319c349c362029f259eb9b75466a1a3e84
dominguezlosa <dominguezlosa@users.noreply.github.com>
2016-01-13 18:11:32 +0100

    Update AdapterFactory.java

--- Projecte/src/showscom/domainLayer/factories/AdapterFactory.java
+++ Projecte/src/showscom/domainLayer/factories/AdapterFactory.java
@@ -5,24 +5,57 @@ import showscom.domainLayer.adapters.CurrencyConvertorAdapter;
 import showscom.domainLayer.adapters.IBankServiceAdapter;
 import showscom.domainLayer.adapters.ICurrencyConvertorAdapter;
 
+/**
+ * Gestiona la creació dels adapters per connectar-nos amb els serveis
+ *
+ */
 public class AdapterFactory {
+	/**
+	 * Instancia del AdapterFactory
+	 */
 	private static AdapterFactory instance = new AdapterFactory();
+
+	/**
+	 * Instancia del BankServiceAdapter
+	 */
 	private static IBankServiceAdapter bankServiceAdap;
+
+	/**
+	 * Instancia del CurrencyConvertorAdapter
+	 */
 	private static ICurrencyConvertorAdapter currencyConvertorAdap;
 
+	/**
+	 * Mètode creador de la classe que crea els dos adapters dels serveis
+	 */
 	private AdapterFactory() {
 		bankServiceAdap = new BankServiceAdapter();
 		currencyConvertorAdap = new CurrencyConvertorAdapter();
 	}
 
+	/**
+	 * Consulta la instància del AdapterFactory
+	 * 
+	 * @return instància de AdapterFactory
+	 */
 	public static AdapterFactory getInstance() {
 		return instance;
 	}
 
+	/**
+	 * Consulta la instància del BankServiceAdapter
+	 * 
+	 * @return instància de BankServiceAdapter
+	 */
 	public static IBankServiceAdapter getBankServiceAdapter() {
 		return bankServiceAdap;
 	}
 
+	/**
+	 * Consulta la instància del CurrencyConvertorAdapter
+	 * 
+	 * @return instància de CurrencyConvertorAdapter
+	 */
 	public static ICurrencyConvertorAdapter getCurrencyConvertorAdapter() {
 		return currencyConvertorAdap;
 	}
