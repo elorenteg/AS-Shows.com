bdde6c72bca2a7f33d763b3ecdd9d607add08d5c
dani <daniel_dolo@hotmail.com>
2016-01-14 20:15:46 +0100

    acentos

--- Projecte/src/test/2JuegoDePruebas.sql
+++ Projecte/src/test/2JuegoDePruebas.sql
@@ -12,14 +12,14 @@ DELETE FROM Sessio;
 INSERT INTO Sessio VALUES ('MATI'), ('TARDA'), ('NIT');
 
 INSERT INTO Local VALUES ('Gran Teatre del Liceu', 'Les Rambles, 51-59'),
-	('Teatre Nacional de Catalunya', 'PlaÃ§a de les Arts, 1'),
-	('Teatro Goya', 'C/ JoaquÃ­n Costa, 68'),
-	('Teatre Lliure de MontjuÃ¯c', 'PlaÃ§a de Margarida Xirgu, 1');
+	('Teatre Nacional de Catalunya', 'Plaça de les Arts, 1'),
+	('Teatro Goya', 'C/ Joaquín Costa, 68'),
+	('Teatre Lliure de Montjuíc', 'Plaça de Margarida Xirgu, 1');
 
 SELECT crea_seients('Gran Teatre del Liceu', 4, 15);
 SELECT crea_seients('Teatre Nacional de Catalunya', 10, 15);
 SELECT crea_seients('Teatro Goya', 10, 10);
-SELECT crea_seients('Teatre Lliure de MontjuÃ¯c', 4, 4);
+SELECT crea_seients('Teatre Lliure de Montjuíc', 4, 4);
 
 INSERT INTO ShowsCom VALUES (1, '2100', '6', 'EUR', 'ES6600190020961234567890', 1);
 INSERT INTO Canvis VALUES (1, 'USD'), (1, 'GBP');
